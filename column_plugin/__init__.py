5ac247c05b3af2a1e9cc30321aa0737d8f0f115a
aathi-star <adityarocksgo@gmail.com>
2025-06-24 13:32:33 +0530

    __init__ file that exposes the main plugin class

---
+++ column_plugin/__init__.py
@@ -0,0 +1,11 @@
+"""
+Column Plugin for Osdag
+Adds Column design module to Compression Member module
+
+Author: Osdag Team
+Version: 0.1.0
+"""
+
+from .plugin import ColumnPlugin
+
+plugin_class = ColumnPlugin
